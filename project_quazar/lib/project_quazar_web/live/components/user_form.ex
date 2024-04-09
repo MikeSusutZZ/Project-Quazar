650ff8a866b5ab313d2abb1bd62830275a164c64
Carson.O <carcango@gmail.com>
2024-04-08 17:50:03 -0700

    Created modular front end template

---
+++ project_quazar/lib/project_quazar_web/live/components/user_form.ex
@@ -0,0 +1,19 @@
+# user_form.ex
+defmodule ProjectQuazarWeb.UserForm do
+  use ProjectQuazarWeb, :live_component
+
+  def render(assigns) do
+    ~H"""
+    <div class="outer-container">
+      <div class="inner-container">
+        <form action="#" method="post" phx-submit="join">
+          <label for="username">Enter your username:</label><br>
+          <input type="text" id="username" name="username"><br><br>
+          <input type="submit" value="Submit">
+        </form>
+        <p class="text-red-500"><%= @error_message %></p>
+      </div>
+    </div>
+    """
+  end
+end
--- project_quazar/lib/project_quazar_web/live/game.ex
+++ project_quazar/lib/project_quazar_web/live/game.ex
@@ -1,3 +1,4 @@
+# game.ex
 defmodule ProjectQuazarWeb.Game do
   use ProjectQuazarWeb, :live_view
   alias ProjectQuazarWeb.Presence
@@ -7,11 +8,10 @@ defmodule ProjectQuazarWeb.Game do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok,
-     socket
-     |> assign(:joined, false)
-     |> assign(:users, %{})
-     |> assign(:error_message, "")}
+    {:ok, socket
+      |> assign(:joined, false)
+      |> assign(:users, %{})
+      |> assign(:error_message, "")}
   end
 
   @impl true
@@ -19,36 +19,29 @@ defmodule ProjectQuazarWeb.Game do
     case Map.has_key?(Presence.list(@presence), username) do
       true ->
         {:reply, %{error: "Username already taken"}, assign(socket, :error_message, "Username already taken")}
-
       false ->
         Presence.track(self(), @presence, username, %{
           points: 0,
         })
-
         Phoenix.PubSub.subscribe(PubSub, @presence)
-
-        {:noreply,
-         socket
-         |> assign(:joined, true)
-         |> assign(:current_user, username)
-         |> handle_joins(Presence.list(@presence))}
+        {:noreply, socket
+          |> assign(:joined, true)
+          |> assign(:current_user, username)
+          |> handle_joins(Presence.list(@presence))}
     end
   end
 
   @impl true
   def handle_info(%Phoenix.Socket.Broadcast{event: "presence_diff", payload: diff}, socket) do
-    {:noreply,
-     socket
-     |> handle_leaves(diff.leaves)
-     |> handle_joins(diff.joins)}
+    {:noreply, socket
+      |> handle_leaves(diff.leaves)
+      |> handle_joins(diff.joins)}
   end
 
   defp handle_joins(socket, joins) do
-    users =
-      joins
+    users = joins
       |> Enum.map(fn {user, %{metas: [meta | _]}} -> {user, meta} end)
       |> Enum.into(%{})
-
     assign(socket, :users, Map.merge(socket.assigns.users, users))
     |> sort_users_by_points()
   end
@@ -60,11 +53,9 @@ defmodule ProjectQuazarWeb.Game do
   end
 
   defp sort_users_by_points(socket) do
-    sorted_users =
-      socket.assigns.users
+    sorted_users = socket.assigns.users
       |> Enum.sort_by(fn {_, %{points: points}} -> -points end)
       |> Enum.into(%{})
-
     assign(socket, :users, sorted_users)
   end
 end
