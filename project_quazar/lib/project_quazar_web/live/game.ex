ce15cb7a042d08e34ebd00d41e3f8ffcfeaa6ee9
Rhys Mahannah <rhys.mahannah@gmail.com>
2024-04-09 18:18:55 -0700

    Rename file; add comments

--- project_quazar/lib/project_quazar_web/live/game.ex
+++ project_quazar/lib/project_quazar_web/live/game.ex
@@ -10,8 +10,8 @@ defmodule ProjectQuazarWeb.Game do
   @impl true
   def mount(_params, _session, socket) do
 
-    # For the all-time high score component, which subscribes to a PubSub
-    # which gets its data from an ETS table.
+    # For the `all-time high scores` component; subscribes to high_scores
+    # PubSub seen in lib > project_quazar_web >channels > `high_scores_channel.ex`
     Phoenix.PubSub.subscribe(ProjectQuazar.PubSub, "high_scores:updates")
     top_scores = fetch_top_scores()
 
@@ -47,6 +47,7 @@ defmodule ProjectQuazarWeb.Game do
       |> handle_joins(diff.joins)}
   end
 
+  # Fetches top scores for all-time high scores component (see below)
   @impl true
   def handle_info(:scores_updated, socket) do
     top_scores = fetch_top_scores()
@@ -81,5 +82,4 @@ defmodule ProjectQuazarWeb.Game do
       {:error, _reason} -> []
     end
   end
-
 end
