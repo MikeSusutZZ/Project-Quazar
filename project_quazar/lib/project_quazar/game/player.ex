b425ad80bb00ee36c9ac659dda2254c5a8b08ce8
Dakaro Mueller <LukarioMC@gmail.com>
2024-04-11 17:13:59 -0700

    Fixed documentation errors

--- project_quazar/lib/project_quazar/game/player.ex
+++ project_quazar/lib/project_quazar/game/player.ex
@@ -7,26 +7,26 @@ defmodule Player do
   - score (int)
   """
   
-  # Player name, Ship, & current score.
+  # Player name, `Ship`, & current score.
   defstruct [:name, :ship, :score]
   
-  @doc 'Creates a new player with a given name and ship (created from `Ship` module)'
+  @doc "Creates a new player with a given name and ship (created from `Ship` module)"
   def new_player(name, ship) do
     %__MODULE__{name: name, ship: ship, score: 0}
   end
   
-  @doc 'Increments the score by an amount.'
+  @doc "Increments the score by an amount."
   def inc_score(%__MODULE__{score: old_score} = player_data, amount) do
     if(amount < 0, do: raise(ArgumentError, message: "Invalid increment amount"))
     %__MODULE__{ player_data | score: (old_score + amount) }
   end
   
-  @doc 'Resets the players current score.'
+  @doc "Resets the players current score."
   def reset_score(%__MODULE__{} = player_data) do
     %__MODULE__{ player_data | score: 0 }
   end
   
-  @doc 'Returns whether this player is alive or not. (> 0 health, or ship destroyed)'
+  @doc "Returns whether the player is alive or not. (> 0 health, or ship destroyed)"
   def alive?(%__MODULE__{ship: ship}) do
     if ship == nil do
       false
@@ -35,35 +35,37 @@ defmodule Player do
     end
   end
   
-  @doc 'Applies an amount of damage to the ships health.'
+  @doc "Applies an amount of damage to the ships health."
   def take_damage(%__MODULE__{ship: ship} = player_data, amount) do
     damaged_ship = Ship.take_damage(ship, amount)
     %__MODULE__{ player_data | ship: damaged_ship }
   end
   
-  @doc 'Respawns the player at a given position and angle (in radians).'
+  @doc "Respawns the player at a given position and angle (in radians)."
   def respawn(%__MODULE__{ship: ship} = player_data, px, py, angle) do
     respawned_ship = Ship.respawn(ship, px, py, angle)
     %__MODULE__{ player_data | ship: respawned_ship }
   end
   
   defimpl Movable.Motion, for: __MODULE__ do
-    @doc 'Moves the players ship according to its kinematics'
+    @doc "Moves the players ship according to its kinematics"
     def move(%@for{ship: ship} = player_data) do
       %@for{ player_data | ship: Movable.Motion.move(ship) }
     end
 
-    @doc 'Accelerates the player in their current direction'
+    @doc "Accelerates the player in their current direction"
     def accelerate(%@for{ship: ship} = player_data, amount) do
       %@for{ player_data | ship: Movable.Motion.accelerate(ship, amount) }
     end
 
-    @doc 'Rotates ship clockwise in degrees (Turns right)'
+    @doc """
+    Rotates ship either clockwise or counter-clockwise in radians.
+    Pass `:cw` for clockwise, `:ccw` for counter-clockwise
+    """
     def rotate(%@for{ship: ship} = player_data, rad, :cw) do
       %@for{ player_data | ship: Movable.Motion.rotate(ship, rad, :cw) }
     end
 
-    @doc 'Rotates ship counter-clockwise in degrees (Turns left)'
     def rotate(%@for{ship: ship} = player_data, rad, :ccw) do
       %@for{ player_data | ship: Movable.Motion.rotate(ship, rad, :ccw) }
     end
--- project_quazar/lib/project_quazar/game/ship.ex
+++ project_quazar/lib/project_quazar/game/ship.ex
@@ -10,7 +10,7 @@ defmodule Ship do
   
   defstruct [:kinematics, :max_health, :health, :bullet_dmg]
 
-  @doc 'Creates a new ship at {x,y}. Health sets maximum and current health, and bullet_dmg sets damage'
+  @doc "Creates a new `Ship` at x,y. Health sets maximum and current health, and bullet_dmg sets damage"
   def new_ship(px, py, angle, health, bullet_dmg) do
     %__MODULE__{
       kinematics: Movable.new_movable(px, py, 0, 0, angle),
@@ -21,42 +21,44 @@ defmodule Ship do
   end
 
   defimpl Movable.Motion, for: __MODULE__ do
-    @doc 'Moves the ship according to its current XY position, acceleration, & velocity'
+    @doc "Moves the ship according to its current XY position, acceleration, & velocity"
     def move(%@for{kinematics: old_position} = ship_data) do
       new_pos = Movable.Motion.move(old_position)
       %@for{ ship_data | kinematics: new_pos }
     end
 
-    @doc 'Accelerates the ship in its current direction'
+    @doc "Accelerates the ship in its current direction"
     def accelerate(%@for{kinematics: old_acceleration} = ship_data, amount) do
       new_acceleration = Movable.Motion.accelerate(old_acceleration, amount)
       %@for{ ship_data | kinematics: new_acceleration }
     end
 
-    @doc 'Rotates ship clockwise in radians (Turns right)'
+    @doc """
+    Rotates ship either clockwise or counter-clockwise in radians.
+    Pass `:cw` for clockwise, `:ccw` for counter-clockwise
+    """
     def rotate(%@for{kinematics: old_rotation} = ship_data, rad, :cw) do
       new_rotation = Movable.Motion.rotate(old_rotation, rad, :cw)
       %@for{ ship_data | kinematics: new_rotation }
     end
 
-    @doc 'Rotates ship counter-clockwise in radians (Turns left)'
     def rotate(%@for{kinematics: old_rotation} = ship_data, rad, :ccw) do
       new_rotation = Movable.Motion.rotate(old_rotation, rad, :ccw)
       %@for{ ship_data | kinematics: new_rotation }
     end
   end
 
-  @doc 'Applies a set amount of damage to the provided ships health'
+  @doc "Applies a set amount of damage to the provided ships health"
   def take_damage(%__MODULE__{health: old_health} = ship_data, amount) do
     %__MODULE__{ ship_data | health: (old_health - amount) }
   end
 
-  @doc 'Returns whether the passed ship is alive (>0 health)'
+  @doc "Returns whether the passed ship is alive (>0 health)"
   def alive?(%__MODULE__{health: health}) do
     health > 0
   end
   
-  @doc 'Respawns the ship at a provided position and angle (in radians)'
+  @doc "Respawns the ship at a provided position and angle (in radians)"
   def respawn(%__MODULE__{max_health: new_health} = ship_data, px, py, angle) do
     new_pos = Movable.new_movable(px, py, 0, 0, angle)
     %__MODULE__{ ship_data | health: new_health, kinematics: new_pos }
