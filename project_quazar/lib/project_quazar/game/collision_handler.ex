86b7007b5b416d1648474a89f3d2bbf399fc23da
Olga Zimina <olga.vic.zimina@gmail.com>
2024-04-13 05:27:24 -0700

    Fix object position mapping

--- project_quazar/lib/project_quazar/game/collision_handler.ex
+++ project_quazar/lib/project_quazar/game/collision_handler.ex
@@ -5,36 +5,36 @@ defmodule CollisionHandler do
 
   @doc """
   Checks for collisions between a list of bullets and ships.
-  Accepts a list of bullets and a list of ships.
-  Returns tuples of collided bullets and ships.
+  Accepts a list of bullets and a list of players.
+  Returns tuples of collided bullets and players.
   """
-  def check_bullet_ship_collisions(bullets, ships) do
+  def check_bullet_ship_collisions(bullets, players) do
     Enum.flat_map(bullets, fn bullet ->
-      Enum.filter(ships, fn ship ->
-        bullet_pos = Movable.Motion.get_pos(bullet.kinematics)
-        ship_pos = Movable.Motion.get_pos(ship.kinematics)
-        collides?(bullet_pos, 1, ship_pos, ship.size) # Assuming bullet size as 1
+      Enum.filter(players, fn player ->
+        %{x: bullet_x, y: bullet_y} = Movable.Motion.get_pos(bullet.kinematics)
+        %{x: ship_x, y: ship_y} = Movable.Motion.get_pos(player.ship.kinematics)
+        collides?({bullet_x, bullet_y}, 1, {ship_x, ship_y}, player.ship.size) # Assuming bullet size as 1
       end)
-      |> Enum.map(fn ship ->
+      |> Enum.map(fn player ->
         # Log the collision
         IO.puts("Collision detected between bullet and ship")
-        {:bullet_ship_collision, bullet, ship}
+        {:bullet_ship_collision, bullet, player}
       end)
     end)
   end
 
   @doc """
-  Checks for collisions between a list of ships.
-  Accepts a list of ships.
-  Returns tuples of collided ships.
+  Checks for collisions between a list of players.
+  Accepts a list of players.
+  Returns tuples of collided players.
   """
-  def check_ship_ship_collisions(ships) do
-    for ship1 <- ships, ship2 <- ships, ship1 != ship2 do
-      pos1 = Movable.Motion.get_pos(ship1.kinematics)
-      pos2 = Movable.Motion.get_pos(ship2.kinematics)
-      if collides?(pos1, ship1.size, pos2, ship2.size) do
+  def check_ship_ship_collisions(players) do
+    for player1 <- players, player2 <- players, player1 != player2 do
+      %{x: ship1_x, y: ship1_y} = Movable.Motion.get_pos(player1.ship.kinematics)
+      %{x: ship2_x, y: ship2_y} = Movable.Motion.get_pos(player2.ship.kinematics)
+      if collides?({ship1_x, ship1_y}, player1.ship.size, {ship2_x, ship2_y}, player2.ship.size) do
         IO.puts("Collision detected between two ships:")
-        {:ship_ship_collision, ship1, ship2}
+        {:ship_ship_collision, player1, player2}
       end
     end
     |> Enum.uniq()
@@ -45,10 +45,10 @@ defmodule CollisionHandler do
   Accepts a list of bullets and ships to be checked
   Returns a tuple of lists of updated bullets and ships
   """
-  def handle_collisions(bullets, ships) do
+  def handle_collisions(bullets, players) do
     IO.puts("Checking for collisions...")
     # Early exit if there are no entities to check for collisions
-    case {bullets, ships} do
+    case {bullets, players} do
       {[], []} ->
         IO.puts("No bullets or ships to check for collisions.")
         {[], []}
@@ -57,23 +57,23 @@ defmodule CollisionHandler do
         IO.puts("No ships available to collide with bullets.")
         {bullets, []}
 
-      {[], _} when length(ships) == 1 ->
+      {[], _} when length(players) == 1 ->
         IO.puts("Only one ship and no bullets; no ship-ship collisions possible.")
-        {[], ships}
+        {[], players}
 
       {[], _} ->
         IO.puts("Multiple ships but no bullets; checking for ship-ship collisions only.")
-        # {[], handle_ship_ship_collisions(ships)}
+        # {[], handle_ship_ship_collisions(players)}
         {[], handle_ship_ship_collisions()}
 
       {_ , _} ->
         # Normal case: Handle both bullet-ship and ship-ship collisions
-        bullet_ship_collisions = check_bullet_ship_collisions(bullets, ships)
-        # {updated_bullets, updated_ships} = handle_bullet_ship_collisions(bullet_ship_collisions, bullets, ships)
+        bullet_ship_collisions = check_bullet_ship_collisions(bullets, players)
+        # {updated_bullets, updated_updated_players} = handle_bullet_ship_collisions(bullet_ship_collisions, bullets, players)
 
-        ship_ship_collisions = check_ship_ship_collisions(ships)
-        # final_updated_ships = handle_ship_ship_collisions(ship_ship_collisions, ships)
-        # {updated_bullets, final_updated_ships}
+        ship_ship_collisions = check_ship_ship_collisions(players)
+        # final_updated_ships = handle_ship_ship_collisions(ship_ship_collisions, players)
+        # {updated_bullets, final_updated_players}
     end
   end
 
@@ -81,7 +81,7 @@ defmodule CollisionHandler do
   # Checks for collision between two circles given their positions and sizes
   # Accepts positions in the format `{x, y}` and sizes
   # Returns true if there is a collision
-  defp collides?(%{px: px1, py: py1}, size1, %{px: px2, py: py2}, size2) do
+  defp collides?({px1, py1}, size1, {px2, py2}, size2) do
     distance({px1, py1}, {px2, py2}) < (size1 + size2)
   end
 
