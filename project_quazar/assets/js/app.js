3a9c75a2ced804f06f15b54fbabd6c3e69eb7ba6
Sean Sollestre <sean.sollestre@gmail.com>
2024-04-13 22:02:27 -0700

    Move prototype 1 to main dir

--- project_quazar/assets/js/app.js
+++ project_quazar/assets/js/app.js
@@ -16,29 +16,57 @@
 //
 
 // Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
-import "phoenix_html"
+import "phoenix_html";
 // Establish Phoenix Socket and LiveView configuration.
-import {Socket} from "phoenix"
-import {LiveSocket} from "phoenix_live_view"
-import topbar from "../vendor/topbar"
+import { Socket } from "phoenix";
+import { LiveSocket } from "phoenix_live_view";
+import topbar from "../vendor/topbar";
 
-let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
+// Hooks initializer
+let Hooks = {};
+
+// Frontend Prototype 1
+Hooks.MoveCircle = {
+  mounted() {
+    console.log("circle mounted");
+    window.addEventListener("keydown", (e) => {
+      console.log(e);
+      if (e.key === "w") {
+        console.log("up");
+        this.pushEvent("move_up", {});
+      } else if (e.key === "a") {
+        console.log("left");
+        this.pushEvent("move_left", {});
+      } else if (e.key === "s") {
+        console.log("down");
+        this.pushEvent("move_down", {});
+      } else if (e.key === "d") {
+        console.log("right");
+        this.pushEvent("move_right", {});
+      }
+    });
+  },
+};
+
+let csrfToken = document
+  .querySelector("meta[name='csrf-token']")
+  .getAttribute("content");
 let liveSocket = new LiveSocket("/live", Socket, {
+  hooks: Hooks,
   longPollFallbackMs: 2500,
-  params: {_csrf_token: csrfToken}
-})
+  params: { _csrf_token: csrfToken },
+});
 
 // Show progress bar on live navigation and form submits
-topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
-window.addEventListener("phx:page-loading-start", _info => topbar.show(300))
-window.addEventListener("phx:page-loading-stop", _info => topbar.hide())
+topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" });
+window.addEventListener("phx:page-loading-start", (_info) => topbar.show(300));
+window.addEventListener("phx:page-loading-stop", (_info) => topbar.hide());
 
 // connect if there are any LiveViews on the page
-liveSocket.connect()
+liveSocket.connect();
 
 // expose liveSocket on window for web console debug logs and latency simulation:
 // >> liveSocket.enableDebug()
 // >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
 // >> liveSocket.disableLatencySim()
-window.liveSocket = liveSocket
-
+window.liveSocket = liveSocket;
---
+++ project_quazar/lib/project_quazar_web/live/components/test_live.ex
@@ -0,0 +1,92 @@
+# Frontend Prototype 1
+defmodule ProjectQuazarWeb.TestLive do
+  use ProjectQuazarWeb, :live_view
+
+  def mount(_params, _session, socket) do
+    initial_x = 100
+    initial_y = 100
+    json_coords = %{x: initial_x, y: initial_y}
+    {:ok, assign(socket, circle_coords: json_coords, rotation_angle: 0)}
+  end
+
+  def handle_event("move_up", _value, socket) do
+    {:noreply,
+     update(socket, :circle_coords, fn %{x: current_x, y: current_y} ->
+       %{x: current_x, y: current_y - 10}
+     end)}
+
+    dec_y(socket, 0, 10)
+  end
+
+  def handle_event("move_down", _value, socket) do
+    {:noreply,
+     update(socket, :circle_coords, fn %{x: current_x, y: current_y} ->
+       %{x: current_x, y: current_y + 10}
+     end)}
+
+    inc_y(socket, 180, 10)
+  end
+
+  def handle_event("move_left", _value, socket) do
+    {:noreply,
+     update(socket, :circle_coords, fn %{x: current_x, y: current_y} ->
+       %{x: current_x - 10, y: current_y}
+     end)}
+
+    dec_x(socket, 270, 10)
+  end
+
+  def handle_event("move_right", _value, socket) do
+    {:noreply,
+     update(socket, :circle_coords, fn %{x: current_x, y: current_y} ->
+       %{x: current_x + 10, y: current_y}
+     end)}
+
+    inc_x(socket, 90, 10)
+  end
+
+  # movement
+  defp inc_x(socket, rotation, inc) do
+    updated_socket =
+      socket
+      |> update(:rotation_angle, fn _current_angle -> rotation end)
+      |> update(:circle_coords, fn %{x: current_x, y: current_y} ->
+        %{x: current_x + inc, y: current_y}
+      end)
+
+    {:noreply, updated_socket}
+  end
+
+  defp dec_x(socket, rotation, dec) do
+    updated_socket =
+      socket
+      |> update(:rotation_angle, fn _current_angle -> rotation end)
+      |> update(:circle_coords, fn %{x: current_x, y: current_y} ->
+        %{x: current_x - dec, y: current_y}
+      end)
+
+    {:noreply, updated_socket}
+  end
+
+  defp inc_y(socket, rotation, inc) do
+    updated_socket =
+      socket
+      |> update(:rotation_angle, fn _current_angle -> rotation end)
+      |> update(:circle_coords, fn %{x: current_x, y: current_y} ->
+        %{x: current_x, y: current_y + inc}
+      end)
+
+    {:noreply, updated_socket}
+  end
+
+  defp dec_y(socket, rotation, dec) do
+    updated_socket =
+      socket
+      |> update(:rotation_angle, fn _current_angle -> rotation end)
+      |> update(:circle_coords, fn %{x: current_x, y: current_y} ->
+        %{x: current_x, y: current_y - dec}
+      end)
+
+    {:noreply, updated_socket}
+  end
+end
