48ba4aa8de6695459acabe81284d142de472520b
Dakaro Mueller <LukarioMC@gmail.com>
2024-04-14 13:57:40 -0700

    Added logic for inside_damage_zone?

--- project_quazar/lib/project_quazar/game/game_boundary.ex
+++ project_quazar/lib/project_quazar/game/game_boundary.ex
@@ -3,6 +3,8 @@ defmodule Boundary do
   Handles boundary collision logic.
 
   All passed boundaries are expected to be from 0 to X, and 0 to Y.
+
+  Currently there are no checks for circle diagonals, only XY boxes.
   """
 
   # Checks if a passed player is outside of a given bounds
@@ -31,6 +33,24 @@ defmodule Boundary do
       # Check if Player_y - radius is > max_y (e.g. [0, 11] - 1r < 10 for bounds 10)
       y - r > max_y -> true
       # Player is not outside of the bounds, return false.
+      true -> false
+    end
+  end
+
+  @doc "Does a simple check to see if the Players XY is within the damage zone (deadzone)"
+  def inside_damage_zone?(%Player{ship: ship}, %{x: max_x, y: max_y, deadzone: deadzone}) do
+    %{x: x, y: y} = Movable.Motion.get_pos(ship)
+    # Check if the x, y positions overlap (Do not check radius for better deadzone feeling at edges)
+    cond do
+      # Check if Player_x is < minimum_x (e.g. [4, 0] < 5 for deadzone 5)
+      x < 0 + deadzone -> true
+      # Check if Player_y is < minimum_y (e.g. [0, 4] < 5 for deadzone 5)
+      y < 0 + deadzone -> true
+      # Check if Player_x is > max_x (e.g. [11, 0] > 10 for deadzone 5, max 15)
+      x > max_x - deadzone -> true
+      # Check if Player_y is > max_y (e.g. [0, 11] > 10 for deadzone 5, max 15)
+      y > max_y - deadzone -> true
+      # Player is not in damage zone, return false.
       true -> false
     end
   end
