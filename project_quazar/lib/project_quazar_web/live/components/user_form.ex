4410dc09cae39e106352be8fe536e5b2fc52c286
AmarjotS82 <amarjot.sangha28@gmail.com>
2024-04-15 15:27:54 -0700

    All taiwind is now in the css file

--- project_quazar/lib/project_quazar_web/live/components/user_form.ex
+++ project_quazar/lib/project_quazar_web/live/components/user_form.ex
@@ -4,69 +4,70 @@ defmodule ProjectQuazarWeb.UserForm do
 
   def render(assigns) do
     ~H"""
-    <div class="outer-container rounded bg-gray-50 border-4 border-gray-200 p-3">
-      <div class="inner-container w-full flex justify-center items-center">
+    <div class="outer-user-form-container">
+      <div class="inner--user-form-container ">
 
         <form  action="#" method="post" phx-submit="join">
-          <label for="username">Enter your username:</label><br>
-          <input type="text" id="username" name="username"><br><br>
+          <label class="username_form_label" for="username">Enter your username:</label>
+          <input class="username_form_inputbox" type="text" id="username" name="username">
+          <p class="error_message"><%= @error_message %></p>
 
           <h2> Choose your Ship:</h2>
-          <div class="flex flex-row w-full justify-between  mt-3 mr-3">
-            <div class='flex flex-col items-center transition duration-0 ease-in-out transform hover:scale-105 '>
-              <label class='mr-2'>Tank</label>
-              <h1 class="text-green-600">Health: 250</h1>
+          <div class="radio-btn-options-container-row">
+            <div class='radio-button-option-col'>
+              <label class='option_label'>Tank</label>
+              <h1 class="health_text">Health: 250</h1>
               <div>
                 <input type='radio' name='ship' value='tank' checked>
               </div>
             </div>
 
-            <div class='flex flex-col items-center mr-3 ml-3 transition duration-0 ease-in-out transform hover:scale-105'>
-              <label class='mr-2'>Destroyer</label>
-              <h1 class="text-green-600">Health: 150</h1>
+            <div class='radio-button-option-col'>
+              <label class='option_label'>Destroyer</label>
+              <h1 class="health_text">Health: 150</h1>
               <div>
                 <input type='radio' name='ship' value='destroyer' checked>
               </div>
             </div>
 
-            <div class='flex flex-col items-center transition duration-0 ease-in-out transform hover:scale-105'>
-              <label class='mr-2'>Scout</label>
-              <h1 class="text-green-600">Health: 100</h1>
+            <div class='radio-button-option-col'>
+              <label class=''>Scout</label>
+              <h1 class="health_text">Health: 100</h1>
               <div>
                 <input type='radio' name='ship' value='scout' checked>
               </div>
             </div>
           </div>
 
-          <h2 class="mt-2"> Choose your Bullet:</h2>
-          <div class="flex flex-row w-full justify-between mt-3 mr-3">
+          <h2 class="bullet_selection_heading"> Choose your Bullet:</h2>
+          <div class="radio-btn-options-container-row">
 
-            <div class='flex flex-col  items-center transition duration-0 ease-in-out transform hover:scale-105 '>
-              <label class='mr-2'>Heavy</label>
+            <div class='radio-button-option-col '>
+              <label class='option_label'>Heavy</label>
 
               <img src={~p"/images/red_bullet_asset/Red_Bullet.png"} alt="Example Image">
-              <h1 class="text-red-600" >Damage: 35</h1>
-              <h1 class="text-yellow-600" >Speed: Slow</h1>
+              <h1 class="damage_text" >Damage: 35</h1>
+              <h1 class="speed_text" >Speed: Slow</h1>
               <div>
                 <input type='radio' name='bullet' value='heavy' checked>
               </div>
             </div>
 
-            <div class='flex flex-col items-center mr-3 ml-3 transition duration-0 ease-in-out transform hover:scale-105'>
-              <label class='mr-2'>Medium</label>
+            <div class='radio-button-option-col'>
+              <label class='option_label'>Medium</label>
               <img src={~p"/images/green_bullet_asset/Green_Bullet.png"} alt="Example Image">
-              <h1 class="text-red-600">Damage: 20</h1>
-              <h1 class="text-yellow-600">Speed: fast</h1>
+              <h1 class="damage_text">Damage: 20</h1>
+              <h1 class="speed_text">Speed: normal</h1>
               <div>
                 <input type='radio' name='bullet' value='medium' checked>
               </div>
             </div>
 
-            <div class='flex flex-col items-center transition duration-0 ease-in-out transform hover:scale-105'>
-              <label class='mr-2'>Light</label>
+            <div class='radio-button-option-col'>
+              <label>Light</label>
               <img src={~p"/images/purple_bullet_asset/Purple_Bullet.png"} alt="Example Image">
-              <h1 class="text-red-600">Damage: 10</h1>
-              <h1 class="text-yellow-600">Speed: very fast</h1>
+              <h1 class="damage_text">Damage: 10</h1>
+              <h1 class="speed_text">Speed: fast</h1>
               <div>
                 <input type='radio' name='bullet' value='light' checked>
               </div>
@@ -74,9 +75,10 @@ defmodule ProjectQuazarWeb.UserForm do
 
           </div>
 
-          <input class= " mt-4 bg-green-600 p-2 rounded cursor-pointer hover:text-white" type="submit" value="Submit">
+          <input class= "submit_btn" type="submit" value="Submit">
+
         </form>
-        <p class="text-red-500"><%= @error_message %></p>
+
         <span phx-window-keydown="ping_server"></span>
         <span phx-window-keydown="control"></span>
       </div>
