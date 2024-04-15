ba2e14c14ff9fa80720a35ef65d39ecac3b1b969
Ediljohn Joson <ejoson1@my.bcit.ca>
2024-04-14 19:47:20 -0700

    changed styling for screens to be larger

--- project_quazar/lib/project_quazar_web/live/components/help.ex
+++ project_quazar/lib/project_quazar_web/live/components/help.ex
@@ -11,35 +11,37 @@ defmodule ProjectQuazarWeb.Help do
     page_content =
       case current_page do
         1 -> ~S"""
-              <div class="mt-6">
-                <p class="text-white">This is page 1 content.</p>
+              <div class="mt-8">
+                <p class="text-white text-lg">This is page 1 content.</p>
               </div>
               """
         2 -> ~S"""
-              <div class="mt-6">
-                <p class="text-white">This is page 2 content.</p>
+              <div class="mt-8">
+                <p class="text-white text-lg">This is page 2 content.</p>
               </div>
               """
         3 -> ~S"""
-              <div class="mt-6">
-                <p class="text-white">This is page 3 content.</p>
+              <div class="mt-8">
+                <p class="text-white text-lg">This is page 3 content.</p>
               </div>
               """
         _ -> "Invalid page number"
       end
 
     ~H"""
-    <div id="help" class="bg-gray-900 p-6 rounded-lg shadow-lg max-w-md mx-auto mt-5">
-      <button phx-click="hide_help" class="bg-gray-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Back</button>
-      <h2 class="text-white text-3xl font-bold text-center mb-6">How To Play</h2>
-      <p class="text-white text-center">Page: <%= current_page %></p>
+    <div id="help" class="bg-gray-900 p-6 rounded-lg shadow-lg max-w-lg mx-auto mt-8">
+      <div class="flex justify-end">
+        <button phx-click="hide_help" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">X</button>
+      </div>
+      <h2 class="text-white text-4xl font-bold text-center mb-8">How To Play</h2>
+      <p class="text-white text-center text-lg">Page: <%= current_page %></p>
       <%= raw(page_content) %>
-      <div class="flex justify-between w-full mt-6">
+      <div class="flex justify-between w-full mt-8">
         <%= if current_page > 1 do %>
-          <button phx-click="previous_page" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Previous</button>
+          <button phx-click="previous_page" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-lg text-lg">Previous</button>
         <% end %>
         <%= if current_page < 3 do %>
-          <button phx-click="next_page" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Next</button>
+          <button phx-click="next_page" class="bg-green-500 hover:bg-green-700 text-white font-bold py-3 px-6 rounded-lg text-lg">Next</button>
         <% end %>
       </div>
     </div>
--- project_quazar/lib/project_quazar_web/live/components/start_screen.ex
+++ project_quazar/lib/project_quazar_web/live/components/start_screen.ex
@@ -1,17 +1,13 @@
 defmodule ProjectQuazarWeb.Start do
   use ProjectQuazarWeb, :live_component
 
-  def mount(_params, _session, socket) do
-    {:ok, assign(socket, show_high_scores: false)}
-  end
-
   def render(assigns) do
     ~H"""
-    <div class="bg-gray-900 p-6 rounded-lg shadow-lg max-w-md mx-auto">
-      <h2 class="text-white text-3xl font-bold text-center mb-6">Project Quazar</h2>
-      <div class="flex flex-col space-y-4 items-center">
-        <button phx-click="show_start_game" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded" phx-submit="start">Start Game</button>
-        <button phx-click="show_help" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">How To Play</button>
+    <div class="bg-gray-900 p-6 rounded-lg shadow-lg max-w-lg mx-auto">
+      <h2 class="text-white text-4xl font-bold text-center mb-8">Project Quazar</h2>
+      <div class="flex flex-col space-y-6 items-center">
+        <button phx-click="show_start_game" class="bg-green-500 hover:bg-green-700 text-white font-bold py-3 px-6 rounded-lg text-lg">Start Game</button>
+        <button phx-click="show_help" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-lg text-lg">How To Play</button>
       </div>
     </div>
     """
