56a48b52de7f7e9ba2ad3b9c06108ba5bb3774e0
Ediljohn Joson <ejoson1@my.bcit.ca>
2024-04-18 20:18:22 -0700

    moved styling to app.cs; other styling fixes

--- project_quazar/lib/project_quazar_web/live/components/help.ex
+++ project_quazar/lib/project_quazar_web/live/components/help.ex
@@ -12,79 +12,79 @@ defmodule ProjectQuazarWeb.Help do
       case current_page do
         1 -> ~S"""
               <div class="mt-8">
-                <h3 class="text-white text-lg font-bold text-center mb-4">Introduction</h3>
-                <p class="text-white text-lg">
+                <h3 class="help-heading">Introduction</h3>
+                <p class="help-text">
                   Welcome to Project Quazar! Control your ship and navigate through space, shooting other player's ships to survive and to rack up the highest score!
                 </p>
               </div>
               """
               2 -> ~S"""
               <div class="mt-8">
-                <h3 class="text-white text-lg font-bold text-center mb-4">Ship Information / Controls</h3>
-                <p class="text-white text-lg">
+                <h3 class="help-heading">Ship Information / Controls</h3>
+                <p class="help-text">
                   Before you begin, you'll need to name your ship and choose a ship and weapon type!
                 </p>
                 <div class="mt-4">
-                  <h4 class="text-white text-lg font-bold mb-2 text-center">Ship Types:</h4>
+                  <h4 class="info-heading">Ship Types:</h4>
                   <div class="flex justify-between">
                     <div>
                       <h5 class="type-heading">Scout</h5>
-                      <p class="text-white italic text-center">100 HP</p>
-                      <p class="text-white text-lg">A fast but weak ship</p>
+                      <p class="help-health-text">100 HP</p>
+                      <p class="help-text">A fast but weak ship</p>
                     </div>
                     <div>
                       <h5 class="type-heading">Destroyer</h5>
-                      <p class="text-white italic text-center">150 HP</p>
-                      <p class="text-white text-lg">An all around ship</p>
+                      <p class="help-health-text">150 HP</p>
+                      <p class="help-text">An all around ship</p>
                     </div>
                     <div>
                       <h5 class="type-heading">Destroyer</h5>
-                      <p class="text-white italic text-center">250 HP</p>
-                      <p class="text-white text-lg">A slow and sturdy ship</p>
+                      <p class="help-health-text">250 HP</p>
+                      <p class="help-text">A slow and sturdy ship</p>
                     </div>
                   </div>
                 </div>
                 <div class="mt-4">
-                  <h4 class="text-white text-lg font-bold mb-2 text-center">Weapon Types:</h4>
+                  <h4 class="info-heading">Weapon Types:</h4>
                   <div class="flex justify-between">
                     <div>
                       <h5 class="type-heading">Light</h5>
                       <ul>
-                        <li class="text-white text-md">Damage: 10</li>
-                        <li class="text-white text-md">Reload: 0.75s</li>
-                        <li class="text-white text-md">Velocity: High</li>
+                        <li class="text-white">Damage: 10</li>
+                        <li class="text-white">Reload: 0.75s</li>
+                        <li class="text-white">Velocity: High</li>
                       </ul>
                     </div>
                     <div>
                       <h5 class="type-heading">Medium</h5>
                       <ul>
-                        <li class="text-white text-md">Damage: 20</li>
-                        <li class="text-white text-md">Reload: 1.1s</li>
-                        <li class="text-white text-md">Velocity: Medium</li>
+                        <li class="text-white">Damage: 20</li>
+                        <li class="text-white">Reload: 1.1s</li>
+                        <li class="text-white">Velocity: Medium</li>
                       </ul>
                     </div>
                     <div>
                       <h5 class="type-heading">Heavy</h5>
                       <ul>
-                        <li class="text-white text-md">Damage: 35</li>
-                        <li class="text-white text-md">Reload: 1.5s</li>
-                        <li class="text-white text-md">Velocity: Slow</li>
+                        <li class="text-white">Damage: 35</li>
+                        <li class="text-white">Reload: 1.5s</li>
+                        <li class="text-white">Velocity: Slow</li>
                       </ul>
                     </div>
                   </div>
                 </div>
                 <div class="mt-4">
-                  <h4 class="text-white text-lg font-bold text-center">Controls:</h4>
-                  <p class="control-item">W - Accelerate</p>
-                  <p class="control-item">A/D - Turn Left/Right</p>
-                  <p class="control-item">S - Decelerate</p>
-                  <p class="control-item">Space - Shoot</p>
+                  <h4 class="info-heading">Controls:</h4>
+                  <p class="control-item"><span class="font-bold">W</span> - Accelerate</p>
+                  <p class="control-item"><span class="font-bold">A/D</span> - Turn Left/Right</p>
+                  <p class="control-item"><span class="font-bold">S</span> - Decelerate</p>
+                  <p class="control-item"><span class="font-bold">Space</span> - Shoot</p>
                 </div>
               </div>
               """
         3 -> ~S"""
               <div class="mt-8">
-                <h3 class="text-white text-lg font-bold text-center mb-4">Game Mechanics</h3>
+                <h3 class="help-heading">Game Mechanics</h3>
                 <p class="text-white text-lg mb-4">
                   Shoot other players to survive the longest! You'll gain more points depending on how long you survive.
                 </p>
@@ -105,7 +105,6 @@ defmodule ProjectQuazarWeb.Help do
         <button phx-click="hide_help" class="button-tertiary">X</button>
       </div>
       <h2 class="startup-title">How To Play</h2>
-      <p class="text-white text-center text-lg">Page: <%= current_page %></p>
       <%= raw(page_content) %>
       <div class="flex justify-between w-full mt-8">
         <%= if current_page > 1 do %>
@@ -115,6 +114,7 @@ defmodule ProjectQuazarWeb.Help do
           <button phx-click="next_page" class="button-primary">Next</button>
         <% end %>
       </div>
+      <p class="text-white text-center text-lg">Page: <%= current_page %></p>
     </div>
     """
   end
